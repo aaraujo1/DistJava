6a286bbd6ae8231ee23aa554f5461c7fcb20174c
André Araujo <aaraujo1@my.wctc.edu>
2019-04-17 19:06:53 -0500

    Finished lecture

---
+++ storespring/src/main/java/us/aaraujo1/storespring/StorespringApplication.java
@@ -0,0 +1,13 @@
+package us.aaraujo1.storespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorespringApplication.class, args);
+    }
+
+}
